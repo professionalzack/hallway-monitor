fbff948ef855dfb4ad23f5648b79c1aaec0a2107
zack <zack.etc@gmail.com>
2019-08-29 23:06:37 -0700

    heroku configs

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+// const config = require('./config.json');
+const prefix = process.env.prefix;
+const token = process.env.BOT_TOKEN;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(config.token);
+client.login(token);
 
 client.on('message', message => {
   const vamp = message.guild.channels.find(ch => ch.id === '585663856404070430');
-  if (message.content.startsWith(`${config.prefix}roast`)) {
+  if (message.content.startsWith(`${prefix}roast`)) {
     message.channel.send(`${message.author}, take that shit to ${vamp.toString()}. you know better`);
   }
 });
